4ff7192a2cb4230114a12556de9239994b928606
Said Sef <6002187+saidsef@users.noreply.github.com>
2025-05-21 01:08:59 +0400

    fix: package setup and install (#18)

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="mcp-saidsef-resume",
+    name="mcp-github-pr-issue-analyser",
     version="1.0.1",
     author="saidsef",
-    description="MCP Said Sef Resume and Socials",
+    description="MCP GitHub PR Analyser and Issue Manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://saidsef.co.uk/saidsef/mcp-said-sef-resume",
+    url="https://github.com/saidsef/mcp-github-pr-issue-analyser",
     packages=find_packages(),
     py_modules=["github_integration", "issues_pr_analyser"],
     classifiers=[
