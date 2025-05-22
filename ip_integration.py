2179e0bcc4605637fbcf1b8aa116e2974b86b431
Said Sef <6002187+saidsef@users.noreply.github.com>
2025-05-22 23:05:21 +0400

    chore: enhance function documentation and add PR comment automation (#21)
    * chore: add pr comment after issue is create
    
    Also, update all functions documentations
    
    * fix: typo github integration
    
    * chore: update package builder
    
    * chore: add pr comment function
    
    * chore: add uv to build requirements
    
    * chore: typo fix on add_pr_comments
    
    * chore: removed redundant section

--- ip_integration.py
+++ ip_integration.py
@@ -32,8 +32,12 @@ class IPIntegration:
   def __init__(self, ipv4_api_url: str = None, ipv6_api_url: str = None) -> None:
     """
     Initialize the IPIntegration class.
-    :param ip_api_url: The URL of the IP API to use. If None, defaults to "https://ipinfo.io/json".
+
+    :param ipv4_api_url: Optional custom API URL for IPv4 information.
+    :param ipv6_api_url: Optional custom API URL for IPv6 information.
     """
+    self.ipv4_api_url = None
+    self.ipv6_api_url = None
     if ipv4_api_url is None or ipv6_api_url is None:
         self.ipv4_api_url = "https://ipinfo.io/json"
         self.ipv6_api_url = "https://v6.ipinfo.io/json"
@@ -42,19 +46,25 @@ class IPIntegration:
         self.ipv6_api_url = ipv6_api_url
 
   def get_info(self, url: str) -> Dict[str, Any]:
-      """
-      Get information about an IP address.
-      :param url: The URL of the IP API to use.
-      :return: A dictionary containing the IP information.
-      """
-      try:
-          response = requests.get(url)
-          response.raise_for_status()
-          return response.json()
-      except requests.RequestException as e:
-          logging.error(f"Error fetching IP info: {e}")
-          logging.debug(traceback.format_exc())
-          return {}
+    """
+    Fetches information from the specified URL using an HTTP GET request.
+    Args:
+        url (str): The URL to send the GET request to.
+    Returns:
+        Dict[str, Any]: The JSON response parsed into a dictionary if the request is successful.
+        Returns an empty dictionary if the request fails or an exception occurs.
+    Error Handling:
+        Logs an error message and stack trace if a requests.RequestException is raised during the HTTP request.
+    """
+      
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        logging.error(f"Error fetching IP info: {e}")
+        logging.debug(traceback.format_exc())
+        return {}
 
   def get_ipv4_info(self) -> Dict[str, Any]:
       """
@@ -73,21 +83,28 @@ class IPIntegration:
           return {}
 
   def get_ipv6_info(self) -> Dict[str, Any]:
-      """
-      Get information about an IPv6 address.
-      :return: A dictionary containing the IPv6 information.
-      """
-      # Override the allowed_gai_family method to use IPv6
-      def allowed_gai_family():
-          return socket.AF_INET6
-      urllib3_connection.allowed_gai_family = allowed_gai_family
-      try:
-          ipv6 = self.get_info(self.ipv6_api_url)
-          if not ipv6:
-              logging.error("No IPv6 information found.")
-              return {}
-          return ipv6
-      except requests.RequestException as e:
-          logging.error(f"Error fetching IPv6 info: {e}")
-          logging.debug(traceback.format_exc())
-          return {}
+    """
+    Retrieves IPv6 information from a specified API endpoint.
+    This method temporarily overrides the `allowed_gai_family` method to force the use of IPv6 when making network requests.
+    It then attempts to fetch IPv6-related information from the configured API URL.
+    Returns:
+        dict: A dictionary containing IPv6 information if the request is successful.
+              Returns an empty dictionary if no information is found or if an error occurs.
+    Error Handling:
+        Logs an error message and returns an empty dictionary if a `requests.RequestException` is raised during the fetch operation.
+        Also logs the full traceback at the debug level for troubleshooting.
+    """
+    # Override the allowed_gai_family method to use IPv6
+    def allowed_gai_family():
+        return socket.AF_INET6
+    urllib3_connection.allowed_gai_family = allowed_gai_family
+    try:
+        ipv6 = self.get_info(self.ipv6_api_url)
+        if not ipv6:
+            logging.error("No IPv6 information found.")
+            return {}
+        return ipv6
+    except requests.RequestException as e:
+        logging.error(f"Error fetching IPv6 info: {e}")
+        logging.debug(traceback.format_exc())
+        return {}
